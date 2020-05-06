5791142bb1ef634945fa8ded0694f4907fbfa82f
github username <github email address>
2020-05-06 11:08:13 -0400

    third

--- src/App.js
+++ src/App.js
@@ -20,24 +20,59 @@ class App extends React.Component {
 	}
 
 	handleChange = (e) => {
-		this.setState(
-			{
-				newPoem: {
-					[e.target.name]: e.target.value,
-				},
+		this.setState({
+			newPoem: {
+				[e.target.name]: e.target.value,
 			},
-			() => console.log(this.state.newPoem)
-		);
+		});
+	};
+
+	// handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	console.log(e.target);
+	// 	fetch("http://localhost:6001/poems", {
+	// 		method: "POST",
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 			Accepts: "application/json",
+	// 		},
+	// 		body: JSON.stringify({
+	// 			newPoem: {
+	// 				author: e.target.value,
+	// 				title: e.target.value,
+	// 				content: e.target.value,
+	// 			},
+	// 		}),
+	// 	})
+	// 		.then((response) => response.json())
+	// 		.then((poem) => {
+	// 			this.setState({
+	// 				poems: [...this.state.poems, poem],
+	// 				newPoem: [],
+	// 			});
+	// 		});
+	// };
+	addnewPoem = (poem) => {
+		this.setState({
+			poems: [...this.state.poems, poem],
+		});
+	};
+
+	displayForm = () => {
+		this.setState((previousState) => {
+			return { hideForm: !previousState.hideForm };
+		});
 	};
 	render() {
 		return (
 			<div className="app">
 				<div className="sidebar">
-					<button>Show/hide new poem form</button>
-					{true && (
+					<button onClick={this.displayForm}>Show/hide new poem form</button>
+					{this.state.hideForm && (
 						<NewPoemForm
-							newPoem={this.state.newPoem}
-							handleChange={this.handleChange}
+							addnewPoem={this.addnewPoem}
+							// handleChange={this.handleChange}
+							// handleSubmit={this.handleSubmit}
 						/>
 					)}
 				</div>
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,27 +1,60 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+	state = {
+		newPoem: [],
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			newPoem: {
+				[e.target.name]: e.target.value,
+			},
+		});
+	};
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target.value);
+		fetch("http://localhost:6001/poems", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Accepts: "application/json",
+			},
+			body: JSON.stringify({
+				author: e.target.value,
+				title: e.target.value,
+				content: e.target.value,
+			}),
+		})
+			.then((response) => response.json())
+			.then((poem) => {
+				this.props.addnewPoem(poem);
+			});
+	};
+
 	render() {
-		let { author, title, content } = this.props.newPoem;
+		let { author, title, content } = this.state.newPoem;
+
 		return (
-			<form className="new-poem-form">
+			<form className="new-poem-form" onSubmit={this.handleSubmit}>
 				<input
 					placeholder="Title"
 					name="Title"
 					value={title}
-					onChange={this.props.handleChange}
+					onChange={this.handleChange}
 				/>
 				<input
 					placeholder="Author"
 					name="Author"
 					value={author}
-					onChange={this.props.handleChange}
+					onChange={this.handleChange}
 				/>
 				<textarea
 					placeholder="Write your masterpiece here..."
 					name="Content"
 					value={content}
-					onChange={this.props.handleChange}
+					onChange={this.handleChange}
 					rows={10}
 				/>
 				<input type="submit" value="Share your masterpiece" />
