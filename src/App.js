56df1c7ecd0faaff2f8829d68985cd3907fd4f2b
osgood gunawan <ogunawan@ucsd.edu>
2020-05-06 10:24:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,44 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+
+  state={
+    poems:[],
+    toggle: false
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/poems')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        poems: data
+      })
+    })
+  }
+
+  handleToggle=()=>{
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
+
+  addForm=(newpoems)=>{
+    this.setState({
+      poems:[...this.state.poems,newpoems]
+    })
+  }
+
   render() {
+    // console.log(this.state)
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={()=>this.handleToggle()}>Show/hide new poem form</button>
+          {this.state.toggle && <NewPoemForm addForm={this.addForm}/>}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.poems}/>
       </div>
     );
   }
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,9 +1,50 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+
+  state={
+    title:'',
+    author:'',
+    content:'',
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+  
+  handleSubmit=(e)=>{
+    e.preventDefault()
+
+    fetch('http://localhost:6001/poems',{
+      method: "POST",
+      headers:{
+        "content-type" :"application/json",
+        accepts: "application/json"
+      },
+      body: JSON.stringify({
+          title:this.state.title,
+          author: this.state.author,
+          content: this.state.content
+      })
+    })
+    .then(resp =>resp.json())
+    .then(newPoem => this.props.addForm(newPoem))
+    .then(this.setState({
+      state ={
+        title:'',
+        author:'',
+        content:'',
+      }
+    }))
+  }
+
+
   render() {
+    // let toggled=this.state.toggle && 
     return (
-      <form className="new-poem-form">
+      <form className="new-poem-form" >
         <input placeholder="Title" />
         <input placeholder="Author" />
         <textarea placeholder="Write your masterpiece here..." rows={10} />
