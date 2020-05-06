9ffee8c16aceb059b0db0cc6a04e8667cb5ac741
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-05-06 11:02:08 -0400

    sorry few more  changes

--- src/Poem.js
+++ src/Poem.js
@@ -17,7 +17,7 @@ class Poem extends React.Component {
         <h3>Title: {this.props.poem.title}</h3>
         <p>Content: {this.props.poem.content}</p>
         <p>
-        <strong>- By Author{this.props.poem.author}</strong>
+        <strong>- By Author: {this.props.poem.author}</strong>
         </p>
         {this.state.read ? <button onClick={this.handleClick}>Read</button> : <button onClick={this.handleClick}>Mark as read</button>}
       </div>
